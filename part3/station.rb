34b9fa2bc7758b0388be1d41566763bf0638474f
alokhina <Kurakura1>
2018-11-14 18:14:24 +0200

    2

--- part3/station.rb
+++ part3/station.rb
@@ -1,6 +1,6 @@
 # Класс Station (Станция):
 #
-#     Имеет название, которое указывается при ее создании
+# Имеет название, которое указывается при ее создании
 # Может принимать поезда (по одному за раз)
 # Может показывать список всех поездов на станции, находящиеся в текущий момент
 # Может показывать список поездов на станции по типу (см. ниже): кол-во грузовых, пассажирских
@@ -8,36 +8,34 @@
 
 class Station
 
-  attr_reader :name, :train
-  # atttr_writer :train
+  attr_reader :name, :trains
 
-  def initialize(name)
+  def initialize(name, *trains)
     @name = name
-    @trains = []
-
+    # @trains = {trains: type}
+    @trains = trains
   end
 
-
-  def new_train( train)
-
-  trains << train
-
+  def new_train(train)
+    trains << train
   end
 
-  def train_online(trains)
-
-    puts "Trains: #{@train}"
+  def train_online
+    puts "Trains: #{@trains}"
   end
 
+  def remove_train (train)
+    trains.delete(train)
+  end
 
-  # def train_type(type)
-  #   trains.select{|train| train.type == type }.join('')
-  # end
-  #
+  def train_with_one_type(tp)
+   one_type_trains = trains.select{|trains, type| type == tp}
+    puts"#{one_type_trains.keys}"
+  end
 
 
   def remove_train (train)
-    remove_instance_variable(:@train)
+    trains.delete(train)
   end
 
 end
--- part3/train.rb
+++ part3/train.rb
@@ -1,35 +1,76 @@
-# Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса
+# Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов,
+# эти данные указываются при создании экземпляра класса
 # Может набирать скорость
 # Может показывать текущую скорость
 # Может тормозить (сбрасывать скорость до нуля)
 # Может показывать количество вагонов
 # Может прицеплять/отцеплять вагоны (по одному вагону за операцию, метод просто увеличивает или уменьшает количество вагонов).
 # Прицепка/отцепка вагонов может осуществляться только если поезд не движется.
-#     Может принимать маршрут следования (объект класса Route)
+# Может принимать маршрут следования (объект класса Route)
 # Может перемещаться между станциями, указанными в маршруте.
-#     Показывать предыдущую станцию, текущую, следующую, на основе маршрута
-
-
+# Показывать предыдущую станцию, текущую, следующую, на основе маршрута
 
 
 class Train
 
-  def initialize(number, type, count_cars)
+  # attr_reader :route
 
+  def initialize(number, type, count_cars, *route)
     @number =number
     @type = type
-    @count_cars = count_cars
-
+    @count_cars = count_cars.to_i
+    @station_id = 0
+    @route = route
   end
 
   def speed=(speed)
-    @speed = speed
-
-
+    @speed = speed.to_i
   end
 
-  def speed
+  def show_speed
+    puts "#{@speed}"
+  end
 
+  def brake
+    @speed = 0
+  end
+
+  def show_cars
+    puts "#{@count_cars}"
+  end
+
+  def hook_cars
+    @count_cars = @count_cars + 1 if @speed == 0
+  end
+
+  def unhook_cars
+    if @speed == 0
+      @count_cars = @count_cars - 1
+    end
+  end
+
+  def train_route(*route)
+    @route = route
+  end
+
+  def train_station
+    route[@station_id]
+  end
+
+  # def to_next_station
+  #   route[@station_id+1]
+  # end
+
+  def next_station
+    route[@station_id+1]
+  end
+
+  def previous_station
+    route[@station_id-1]
+  end
+
+  def move
+    @station_id+=1
   end
 
 end
