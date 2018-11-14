1d05f208981366e8aefabb8543638a20babd1523
alokhina <Kurakura1>
2018-11-14 15:21:38 +0200

    2

--- part3/route.rb
+++ part3/route.rb
@@ -1,6 +1,6 @@
 # Имеет начальную и конечную станцию, а также список промежуточных станций.
 # Начальная и конечная станции указываютсся при создании маршрута, а промежуточные могут добавляться между ними.
-#     Может добавлять промежуточную станцию в список
+# Может добавлять промежуточную станцию в список
 # Может удалять промежуточную станцию из списка
 # Может выводить список всех станций по-порядку от начальной до конечной
 
@@ -12,17 +12,15 @@ class Route
   end
 
   def intermediate_station (station)
-    stations.insert(-2, station)
-
+    @stations.insert(-2, station)
   end
 
   def delete_intr_st(station)
-    stations.delete(station)
+    @stations.delete(station)
   end
 
   def show
-    print "{@stations}"
-
+    puts "#{@stations}"
   end
 
 end
