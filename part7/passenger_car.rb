18674b0d5f9064ebb80aec62296217585e95d8a3
alokhina <Kurakura1>
2018-11-26 11:53:05 +0200

    123

--- part7/passenger_car.rb
+++ part7/passenger_car.rb
@@ -4,14 +4,15 @@ class PassengerCar < Car
     @count_seats = count_seats
     @take_seat = 0
   end
-  def take_seats(n)
-    self.take_seat = @take_seat + n
+  def take_seats
+    raise "There are no more empty seats" if @take_seat == @count_seats
+    @take_seat += 1
   end
   def occupied_seats
     puts "Occupied seats: #{ @take_seat }"
   end
   def free_seats
-    free_seat = @count_seats - take_seat
-    puts "Free seats: #{ @free_seat }"
+    free_seats = @count_seats - @take_seat
+    puts "Free seats: #{free_seats}"
   end
 end
